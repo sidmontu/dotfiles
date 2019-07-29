6193b5cb152809e29c8ab22de8633db760990390
Sid <siddhartha.siddhartha@sydney.edu.au>
2019-07-29 14:15:49 +1000

    add comment to explain vivado script

--- run_vivado.sh
+++ run_vivado.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/zsh
+#!/bin/zsh
+
+# Latest versions of Vivado (2018.3 and above) have issues running when your
+# system has multiple displays. This script is a weird hack to trick Vivado
+# into thinking there's only one display monitor attached to your system.
+# Weird, I know. Go figure.
+
+VIVADO_VER=2018.3
 
 Xephyr :2 -screen 1910x1010&
 DISPLAY=:2
 openbox&
-source /opt/Xilinx/Vivado/2018.3/settings64.sh
-xterm
-# vivado
+source /opt/Xilinx/Vivado/${VIVADO_VER}/settings64.sh
+vivado
