8d27df89bddd2e0b1e8525b39a6eedf33be7c179
Sid <sidmontu@vlan-2675-10-17-53-136.staff.wireless.sydney.edu.au>
2019-07-29 15:08:46 +1000

    more more robust bootstrapping

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,9 +1,20 @@
 #!/bin/zsh
 
-sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# install oh-my-zsh if its not installed
+if [ -d "$HOME/.oh-my-zsh" ]
+then
+    echo "oh-my-zsh is already installed. Skipping."
+else
+    sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# oh my zsh creates a .zshrc template, but luckily saves our existing zshrc, so we have to overwrite it back
-mv .zshrc.pre-oh-my-zsh .zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-config config --local status.showUntrackedFiles no
+    # oh my zsh creates a .zshrc template, but luckily saves our existing zshrc, so we have to overwrite it back
+    mv .zshrc.pre-oh-my-zsh .zshrc
+fi
+
+if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]
+then
+    echo "zsh-autosuggestions plugin already installed. Skipping."
+else
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
--- .zshrc
+++ .zshrc
@@ -154,3 +154,4 @@ fi
 
 # .cfg dotfiles alias
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
